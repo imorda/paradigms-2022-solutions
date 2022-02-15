8675d448e6ccf5632515814273c9259e7e4e8610
Georgiy Korneev <kgeorgiy@kgeorgiy.info>
2022-02-15 15:02:53 +0300

    Checked by an instructor

--- java-solutions/search/BinarySearchMin.java
+++ java-solutions/search/BinarySearchMin.java
@@ -3,77 +3,77 @@ package search;
 public class BinarySearchMin {
 
     // Pre:  array initialized, \exists x=0..array.length : \forall i=0..x-1 array[x] - array[i] <= 0, \forall i=x..array.length-1  array[i] - array[x] > 0,
-    //       lBorder >= -1 exclusive search left border, array[lBorder + 1] - array[lBorder] <= 0 (or undefined),
-    //       rBorder < array.length inclusive search right border, array[rBorder + 1] - array[rBorder] > 0 (or undefined),
-    //       rBorder > lBorder
+    //       l >= -1 exclusive search left border, array[l + 1] - array[l] <= 0 (or undefined),
+    //       r < array.length inclusive search right border, array[r + 1] - array[r] > 0 (or undefined),
+    //       r > l
     // Post: \forall i=0..R-1  array[R] - array[i] <= 0, \forall i=R..array.length-1  array[i] - array[R] > 0, array[R] exists.
-    public static int binSearchMinIter(final int[] array, int lBorder, int rBorder) {
+    public static int binSearchMinIter(final int[] array, int l, int r) {
         // B: \exists x : array[x+1] - array[x] <= 0 \implies \forall i=0..x array[i+1] - array[i] < 0
         //    \exists x : array[x+1] - array[x] > 0 \implies \forall i=x..array.length-2 array[i+1] - array[i] < 0
 
-        // Inv: rBorder > lBorder, array[lBorder + 1] - array[lBorder] <= 0 (or undefined),
-        //      array[rBorder + 1] - array[rBorder] > 0 (or undefined)
-        while(rBorder - lBorder > 1){
-            // Inv && (rBorder - lBorder > 1)
-            int middle = lBorder + (rBorder - lBorder) / 2;
-            // (rBorder - lBorder > 1) -> ((rBorder - lBorder) / 2 >= 1) -> middle > lBorder,
-            // (lBorder + (rBorder - lBorder) / 2) < (lBorder + (rBorder - lBorder)) = rBorder -> middle < rBorder.
+        // Inv: r > l, array[l + 1] - array[l] <= 0 (or undefined),
+        //      array[r + 1] - array[r] > 0 (or undefined)
+        while(r - l > 1){
+            // Inv && (r - l > 1)
+            int middle = l + (r - l) / 2;
+            // (r - l > 1) -> ((r - l) / 2 >= 1) -> middle > l,
+            // (l + (r - l) / 2) < (l + (r - l)) = r -> middle < r.
 
-            // Inv && (rBorder - lBorder > 1) && lBorder < middle < rBorder
-            if(array[middle + 1] - array[middle] > 0){ //array[middle+1] defined, because middle < rBorder, rBorder is inclusive
+            // Inv && (r - l > 1) && l < middle < r
+            if(array[middle + 1] - array[middle] > 0){ //array[middle+1] defined, because middle < r, r is inclusive
                 // Inv && array[middle + 1] - array[middle] > 0
-                rBorder = middle;
-                // rBorder > lBorder, array[rBorder + 1] - array[rBorder] > 0 => Inv satisfied
+                r = middle;
+                // r > l, array[r + 1] - array[r] > 0 => Inv satisfied
             } else {
                 // Inv && array[middle + 1] - array[middle] <= 0
-                lBorder = middle;
-                // rBorder > lBorder, array[lBorder + 1] - array[lBorder] <= 0 => Inv satisfied
+                l = middle;
+                // r > l, array[l + 1] - array[l] <= 0 => Inv satisfied
             }
-            // (lBorder < middle < rBorder) -> rBorder' - lBorder' < rBorder-lBorder -> loop is NOT endless.
+            // (l < middle < r) -> r' - l' < r-l -> loop is NOT endless.
         }
-        // rBorder > lBorder, array[lBorder + 1] - array[lBorder] <= 0 (or undefined) && B, (rBorder - lBorder <= 1),
-        // array[rBorder + 1] - array[rBorder] > 0 -> (rBorder != lBorder)) && (rBorder > lBorder) ->
-        // -> (rBorder - lBorder = 1) -> array[rBorder] - array[rBorder - 1] > target && B -> \forall x \in [0, rBorder - 1]
+        // r > l, array[l + 1] - array[l] <= 0 (or undefined) && B, (r - l <= 1),
+        // array[r + 1] - array[r] > 0 -> (r != l)) && (r > l) ->
+        // -> (r - l = 1) -> array[r] - array[r - 1] > target && B -> \forall x \in [0, r - 1]
         //      array[x + 1] - array[x] > 0
         // Equiv: \forall \eps > 0 \in N : R-\eps >= 0  array[R] - array[R-\eps] <= 0, array[R+1] - array[R] > 0 (or undefined)
-        return rBorder;
+        return r;
     }
 
     // Pre:  array initialized, \exists x=0..array.length : \forall i=0..x-1 array[x] - array[i] <= 0, \forall i=x..array.length-1  array[i] - array[x] > 0,
-    //       lBorder >= -1 exclusive search left border, array[lBorder + 1] - array[lBorder] <= 0 (or undefined),
-    //       rBorder < array.length inclusive search right border, array[rBorder + 1] - array[rBorder] > 0 (or undefined),
-    //       rBorder > lBorder
+    //       l >= -1 exclusive search left border, array[l + 1] - array[l] <= 0 (or undefined),
+    //       r < array.length inclusive search right border, array[r + 1] - array[r] > 0 (or undefined),
+    //       r > l
     // Post: \forall i=0..R-1  array[R] - array[i] <= 0, \forall i=R..array.length-1  array[i] - array[R] > 0, array[R] exists.
-    public static int binSearchMinRec(final int[] array, final int lBorder, final int rBorder) {
-        // A: rBorder > lBorder, array[lBorder + 1] - array[lBorder] <= 0 (or undefined),
-        //    array[rBorder + 1] - array[rBorder] > 0 (or undefined)
+    public static int binSearchMinRec(final int[] array, final int l, final int r) {
+        // A: r > l, array[l + 1] - array[l] <= 0 (or undefined),
+        //    array[r + 1] - array[r] > 0 (or undefined)
         // B: \exists x : array[x+1] - array[x] <= 0 \implies \forall i=0..x array[i+1] - array[i] < 0
         //    \exists x : array[x+1] - array[x] > 0 \implies \forall i=x..array.length-2 array[i+1] - array[i] < 0
-        if(rBorder - lBorder <= 1){
-            // A && (rBorder - lBorder <= 1) -> lBorder != rBorder -> rBorder - lBorder = 1 ->
-            // array[rBorder] - array[rBorder - 1] > target && B -> \forall x \in [0, rBorder - 1]
+        if (r - l <= 1){
+            // A && (r - l <= 1) -> l != r -> r - l = 1 ->
+            // array[r] - array[r - 1] > target && B -> \forall x \in [0, r - 1]
             //      array[x + 1] - array[x] > 0
             // A satisfies B
             // Equiv: \forall \eps > 0 \in N : R-\eps >= 0  array[R] - array[R-\eps] <= 0, array[R+1] - array[R] > 0 (or undefined)
-            return rBorder;
+            return r;
         } else {
             // A
 
-            int middle = lBorder + (rBorder - lBorder) / 2;
-            // (rBorder - lBorder > 1) -> ((rBorder - lBorder) / 2 >= 1) -> middle > lBorder,
-            // (lBorder + (rBorder - lBorder) / 2) < (lBorder + (rBorder - lBorder)) = rBorder -> middle < rBorder.
+            int middle = l + (r - l) / 2;
+            // (r - l > 1) -> ((r - l) / 2 >= 1) -> middle > l,
+            // (l + (r - l) / 2) < (l + (r - l)) = r -> middle < r.
 
-            // A && (rBorder - lBorder > 1) && lBorder < middle < rBorder
+            // A && (r - l > 1) && l < middle < r
             if (array[middle + 1] - array[middle] > 0) {
-                // middle - lBorder < rBorder - lBorder -> recursion is NOT endless
-                // array[middle + 1] - array[middle] > 0 -> array[rBorder' + 1] - array[rBorder'] > 0 -> this function call
+                // middle - l < r - l -> recursion is NOT endless
+                // array[middle + 1] - array[middle] > 0 -> array[r' + 1] - array[r'] > 0 -> this function call
                 //      meets "Pre" requirements
-                return binSearchMinRec(array, lBorder, middle);
+                return binSearchMinRec(array, l, middle);
             } else {
-                // middle - lBorder < rBorder - lBorder -> recursion is NOT endless
-                // array[middle + 1] - array[middle] > 0 -> array[lBorder' + 1] - array[lBorder'] <= 0 -> this function call
+                // middle - l < r - l -> recursion is NOT endless
+                // array[middle + 1] - array[middle] > 0 -> array[l' + 1] - array[l'] <= 0 -> this function call
                 //      meets "Pre" requirements
-                return binSearchMinRec(array, middle, rBorder);
+                return binSearchMinRec(array, middle, r);
             }
         }
     }
@@ -104,9 +104,9 @@ public class BinarySearchMin {
         // Input is parsed as 'target' and 'array' as ints
         // 'array' is sorted in strict descending order up to some point strict ascending order after. (by the task definition)
 
-        // (lBorder = -1 >= -1, array[-1] undefined) && (rBorder = array.length <= array.length, array[array.length] undefined) ->
+        // (l = -1 >= -1, array[-1] undefined) && (r = array.length <= array.length, array[array.length] undefined) ->
         //     -> search is applied to the whole array
-        // min array.length = 0, 0 > -1 -> rBorder > lBorder
+        // min array.length = 0, 0 > -1 -> r > l
         int result = binSearchMinIter(array, -1, array.length - 1);
         // \forall \eps \in N : result-\eps >= 0    array[result-\eps] > target, array[result] <= target (or undefined) -> result is an answer to print
         System.out.println(array[result]);

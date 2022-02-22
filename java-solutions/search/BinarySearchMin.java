e1621e6f16f5a9cb5f3cc9be52c71fa6592d4b77
Georgiy Korneev <kgeorgiy@kgeorgiy.info>
2022-02-22 13:51:20 +0300

    Checked by an instructor

--- java-solutions/search/BinarySearchMin.java
+++ java-solutions/search/BinarySearchMin.java
@@ -39,18 +39,19 @@ public class BinarySearchMin {
         return r;
     }
 
-    // Pre:  a initialized, \exists x=0..a.length : \forall i=0..x-1 a[x] - a[i] <= 0, \forall i=x..a.length-1  a[i] - a[x] > 0,
-    //       l >= -1 exclusive search left border, a[l + 1] - a[l] <= 0 (or undefined),
-    //       r < a.length inclusive search right border, a[r + 1] - a[r] > 0 (or undefined),
+    // Pre:  \exists x=0..a.length : \forall i=0..x-1 a[x] <= a[i], \forall i=x..a.length-1  a[i] > a[x],
+    //  Let a[-1] = +inf
+    //       l >= -1 exclusive search left border, a[l + 1] <= a[l] (or undefined),
+    //       r < a.length inclusive search right border, a[r + 1] > a[r] (or undefined),
     //       r > l
     //       a elements - integer-formatted decimal numeric string
-    // Post: \forall i=0..R-1  a[R] - a[i] <= 0, \forall i=R..a.length-1  a[i] - a[R] > 0, a[R] exists.
+    // Post: \forall i=0..R-1  a[R] <= a[i], \forall i=R..a.length-1  a[i] > a[R], a[R] exists.
     public static int binSearchMinRec(final String[] a, final int l, final int r) {
-        // Inv: r > l, a[l + 1] - a[l] <= 0 (or undefined),
-        //    a[r + 1] - a[r] > 0 (or undefined)
-        // Monoton: \exists x : a[x+1] - a[x] <= 0 \implies \forall i=0..x a[i+1] - a[i] < 0
+        // Inv: r > l, a[l + 1] <= a[l] (or undefined),
+        //    a[r + 1] > a[r] (or undefined)
+        // Monotone: \exists x : a[x+1] - a[x] <= 0 \implies \forall i=0..x a[i+1] - a[i] < 0
         //    \exists x : a[x+1] - a[x] > 0 \implies \forall i=x..a.length-2 a[i+1] - a[i] < 0
-        if (r - l <= 1){
+        if (r - l <= 1) {
             // InvA && (r - l <= 1) -> l != r -> r - l = 1 ->
             // a[r] - a[r - 1] > target && Monoton -> \forall x \in [0, r - 1]
             //      a[x + 1] - a[x] > 0
@@ -65,9 +66,9 @@ public class BinarySearchMin {
             // (l + (r - l) / 2) < (l + (r - l)) = r -> m < r.
 
             // Inv && (r - l > 1) && l < m < r
-            if(Integer.parseInt(a[m + 1]) - Integer.parseInt(a[m]) > 0){
+            if (Integer.parseInt(a[m + 1]) > Integer.parseInt(a[m])){
                 // m - l < r - l -> recursion is NOT endless
-                // a[m + 1] - a[m] > 0 -> a[r' + 1] - a[r'] > 0 -> this function call
+                // a[m + 1] > a[m] > 0 -> a[r' + 1] > a[r'] -> this function call
                 //      meets "Pre" requirements
                 return binSearchMinRec(a, l, m);
             } else {

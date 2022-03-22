c069f2e8f6de9002dd8bba756565e09999c1a6eb
Тимофей <btima@mail.ru>
2022-03-22 04:03:38 +0300

    + Cli interface

--- java-solutions/expression/generic/GenericTabulator.java
+++ java-solutions/expression/generic/GenericTabulator.java
@@ -3,9 +3,6 @@ package expression.generic;
 import expression.exceptions.ParseException;
 
 public class GenericTabulator implements Tabulator {
-    public static void main(String[] args) {
-    }
-
     @Override
     public Object[][][] tabulate(String mode, String expression, int x1, int x2, int y1, int y2,
                                  int z1, int z2) throws ParseException, IllegalStateException {
@@ -26,5 +23,40 @@ public class GenericTabulator implements Tabulator {
         }
 
         return ans;
+    }
+
+    public static void main(String[] args) {
+        if(args.length < 2){
+            System.err.println("Wrong number of arguments passed. Usage: [-number type] expression");
+            return;
+        }
+        String mode = args[0].substring(1);
+        StringBuilder expression = new StringBuilder();
+        for (int i = 1; i < args.length; i++) {
+            expression.append(args[i]);
+        }
+
+        try {
+            Object[][][] ans = new GenericTabulator().tabulate(mode, expression.toString(),
+                    -2, 2, -2, 2, -2, 2);
+
+            for (Object[][] i : ans) {
+                for (Object[] j : i) {
+                    for (Object k : j) {
+                        System.out.print(k);
+                        System.out.print(' ');
+                    }
+                    System.out.println();
+                }
+                for (int k = 0; k < i[0].length; k++) {
+                    System.out.print("--");
+                }
+                System.out.println();
+            }
+        } catch (ParseException e){
+            System.err.println("Invalid expression passed");
+        } catch (IllegalStateException e){
+            System.err.println("Invalid number mode");
+        }
     }
 }

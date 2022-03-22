4be3316413e08c6bd1e3e7b0be485c9cffd11ef9
Тимофей <btima@mail.ru>
2022-03-22 14:07:52 +0300

    + functionalExpression PieIffAbs implemented

--- javascript-solutions/functionalExpression.js
+++ javascript-solutions/functionalExpression.js
@@ -7,12 +7,27 @@ const add = (left, right) => (x, y, z) => left(x, y, z) + right(x, y, z);
 const subtract = (left, right) => (x, y, z) => left(x, y, z) - right(x, y, z);
 const multiply = (left, right) => (x, y, z) => left(x, y, z) * right(x, y, z);
 const divide = (left, right) => (x, y, z) => left(x, y, z) / right(x, y, z);
+const pi = cnst(Math.PI);
+const e = cnst(Math.E);
+const abs = expr => (x, y, z) => Math.abs(expr(x, y, z));
+const iff = (cond, branch1, branch2) => (x, y, z) => cond(x, y, z) >= 0 ? branch1(x, y, z) : branch2(x, y, z);
 
 
 const parse = str => parseTokenized(str.split(" "));
 
 const binOpsDict = {"+": add, "-": subtract, "*": multiply, "/": divide};
-const unaryOpsDict = {"negate": negate};
+const unaryOpsDict = {"negate": negate, "abs": abs};
+const ternaryOpsDict = {"iff": iff};
+const constsDict = {"pi": pi, "e": e};
+
+const parseTokens = function(n, stack) {
+    let ans = [];
+    for(let i = 0; i < n; i++){
+        ans.push(parseTokenized(stack));
+    }
+    ans.reverse();
+    return ans;
+}
 
 const parseTokenized = function (stack) {
     let current = stack.pop().trim().toLowerCase();
@@ -25,10 +40,13 @@ const parseTokenized = function (stack) {
     if ("xyz".includes(current)) {
         return variable(current);
     } else if (binOpsDict[current] !== undefined) {
-        let tmp = parseTokenized(stack);
-        return binOpsDict[current](parseTokenized(stack), tmp);
+        return binOpsDict[current](...parseTokens(2, stack));
     } else if (unaryOpsDict[current] !== undefined) {
-        return unaryOpsDict[current](parseTokenized(stack));
+        return unaryOpsDict[current](...parseTokens(1, stack));
+    } else if (ternaryOpsDict[current] != undefined){
+        return ternaryOpsDict[current](...parseTokens(3, stack));
+    } else if (constsDict[current] != undefined) {
+        return constsDict[current];
     } else {
         return cnst(parseInt(current));
     }
